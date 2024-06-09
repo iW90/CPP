bf8f6f459e19e13e50d432949c1b7b7ec76d327e
iW90 <indynha.w@gmail.com>
2024-06-09 18:25:08 -0300

    feat: add initial files

---
+++ module_00/ex02/Account.cpp
@@ -0,0 +1,68 @@
+#include "Account.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 16:12:06 by inwagner          #+#    #+#             */
+/*   Updated: 2024/06/09 16:12:07 by inwagner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int Account::getNbAccounts(void)
+{
+    return 0;
+}
+
+int Account::getTotalAmount(void)
+{
+    return 0;
+}
+
+int Account::getNbDeposits(void)
+{
+    return 0;
+}
+
+int Account::getNbWithdrawals(void)
+{
+    return 0;
+}
+
+void Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+}
+
+Account::~Account(void)
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    return false;
+}
+
+int Account::checkAmount(void) const
+{
+    return 0;
+}
+
+void Account::displayStatus(void) const
+{
+}
+
+
+// Private
+void Account::_displayTimestamp(void)
+{
+}
