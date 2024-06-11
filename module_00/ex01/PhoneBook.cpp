d647dc828fdabe93b1a54ed87871fe7156ed36ea
iW90 <indynha.w@gmail.com>
2024-06-10 21:01:49 -0300

    fix: change contact by id display

--- module_00/ex01/PhoneBook.cpp
+++ module_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 18:25:40 by inwagner          #+#    #+#             */
-/*   Updated: 2024/06/10 20:43:59 by inwagner         ###   ########.fr       */
+/*   Updated: 2024/06/10 20:59:22 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,44 +141,14 @@ void PhoneBook::add_new_contact(void) {
 		is out of range or wrong, define a relevant behavior. Otherwise, display the
 		contact information, one field per line.
 */
-void PhoneBook::_print_field(const std::string& field) {
-	std::cout << "|";
-	if (field.length() > 10) {
-		std::cout.width(10);
-		std::cout << std::right << field.substr(0, 9) + "." << std::flush;
-	} else {
-		std::cout.width(10);
-		std::cout << std::right << field << std::flush;
+
+void PhoneBook::display_contacts(void) {
+	if (_phonebook[0].getFirstName().empty()) {
+		std::cout << "Phonebook is empty.\n" << std::endl;
+		return ;
 	}
-}
-
-
-void PhoneBook::_print_contact_by_id(const Contact& contact) {
-	std::cout << "--------------------------------------------------------" << std::endl;
-	_print_field("FirstName");
-	_print_field("LastName");
-	_print_field("Nick");
-	_print_field("Phone");
-	_print_field("Secret");
-	std::cout << "|" << std::endl;
-	std::cout << "--------------------------------------------------------" << std::endl;
-	_print_field(contact.getFirstName());
-	_print_field(contact.getLastName());
-	_print_field(contact.getNickname());
-	_print_field(contact.getPhoneNumber());
-	_print_field(contact.getDarkestSecret());
-	std::cout << "|" << std::endl;
-	std::cout << "--------------------------------------------------------\n" << std::endl;
-}
-
-void PhoneBook::_print_contact(const Contact& contact, const int& id) {
-	std::cout << std::right << "|";
-	std::cout.width(10);
-	std::cout << std::right << id << std::flush;
-	_print_field(contact.getFirstName());
-	_print_field(contact.getLastName());
-	_print_field(contact.getNickname());
-	std::cout << "|" << std::endl;
+	_display_contacts_list(*this);
+	search_contact_by_id();	
 }
 
 void PhoneBook::_display_contacts_list(PhoneBook& phonebook) {
@@ -198,9 +168,31 @@ void PhoneBook::_display_contacts_list(PhoneBook& phonebook) {
 	std::cout << "---------------------------------------------\n" << std::endl;
 }
 
+void PhoneBook::_print_field(const std::string& field) {
+	std::cout << "|";
+	if (field.length() > 10) {
+		std::cout.width(10);
+		std::cout << std::right << field.substr(0, 9) + "." << std::flush;
+	} else {
+		std::cout.width(10);
+		std::cout << std::right << field << std::flush;
+	}
+}
+
+void PhoneBook::_print_contact(const Contact& contact, const int& id) {
+	std::cout << std::right << "|";
+	std::cout.width(10);
+	std::cout << std::right << id << std::flush;
+	_print_field(contact.getFirstName());
+	_print_field(contact.getLastName());
+	_print_field(contact.getNickname());
+	std::cout << "|" << std::endl;
+}
+
 void PhoneBook::search_contact_by_id(void) {
 	std::string input;
 
+	std::cout << "Choose an index: " << std::flush;
 	while (true) {
 		std::getline(std::cin, input);
 		if (input.length() != 1 || input[0] < '1' || input[0] > '8')
@@ -220,11 +212,12 @@ void PhoneBook::search_contact_by_id(void) {
 	_print_contact_by_id(contact);
 }
 
-void PhoneBook::display_contacts(void) {
-	if (_phonebook[0].getFirstName().empty()) {
-		std::cout << "Phonebook is empty.\n" << std::endl;
-		return ;
-	}
-	_display_contacts_list(*this);
-	search_contact_by_id();	
+void PhoneBook::_print_contact_by_id(const Contact& contact) {
+	std::cout << "--------------------------------------------------------" << std::endl;
+	std::cout << "First Name:\t" << contact.getFirstName() << std::endl;
+	std::cout << "Last Name:\t" << contact.getLastName() << std::endl;
+	std::cout << "Nickname:\t" << contact.getNickname() << std::endl;
+	std::cout << "Phone Number:\t" << contact.getPhoneNumber() << std::endl;
+	std::cout << "Darkest Secret:\t" << contact.getDarkestSecret() << std::endl;
+	std::cout << "--------------------------------------------------------\n" << std::endl;
 }
