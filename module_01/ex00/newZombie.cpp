ef539caa325ecbc6dc93c319848325db94917d36
iW90 <indynha.w@gmail.com>
2024-06-11 21:57:46 -0300

    doc: add memory information

--- module_01/ex00/newZombie.cpp
+++ module_01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:35:28 by inwagner          #+#    #+#             */
-/*   Updated: 2024/06/11 21:43:53 by inwagner         ###   ########.fr       */
+/*   Updated: 2024/06/11 21:56:31 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,13 @@ Zombie *Zombie::newZombie(std::string name) {
 		
 	return zomb;
 }
+
+/*
+	Memória Heap:
+
+	A memória heap é uma área de memória mais dinâmica e expansível.
+	As alocações e desalocações de memória na heap são feitas manualmente pelo programador (new).
+	As variáveis ​​na heap têm tempo de vida indefinido, elas persistem até serem explicitamente desalocadas (delete).
+	O acesso à memória heap é um pouco mais lento do que à memória stack, pois envolve alocações e desalocações mais complexas.
+	A memória heap geralmente tem um tamanho muito maior do que a memória stack.
+*/
--- module_01/ex00/randomChump.cpp
+++ module_01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 20:35:21 by inwagner          #+#    #+#             */
-/*   Updated: 2024/06/11 21:43:57 by inwagner         ###   ########.fr       */
+/*   Updated: 2024/06/11 21:55:24 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,13 @@ void Zombie::randomChump(std::string name) {
 	Zombie zomb(name);
 	zomb.announce();
 }
+
+/*
+	Memória Stack:
+
+	A memória stack é uma área de memória reservada para funções e variáveis locais.
+	As variáveis ​​criadas na stack têm um tempo de vida limitado ao escopo da função que as cria.
+	As variáveis ​​são alocadas e desalocadas automaticamente pelo compilador à medida que a execução do programa avança e as funções são chamadas e retornam.
+	O acesso à memória stack é rápido, pois as alocações e desalocações são simplesmente feitas movendo um ponteiro.
+	A memória stack tem tamanho limitado.
+*/
