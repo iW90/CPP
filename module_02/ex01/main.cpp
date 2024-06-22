3e2d69509982ccfb91edfc5b03b953d4fdc5cd9a
iW90 <indynha.w@gmail.com>
2024-06-22 18:36:10 -0300

    feat: add more tests to the main of mod02

--- module_02/ex01/main.cpp
+++ module_02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 19:15:07 by inwagner          #+#    #+#             */
-/*   Updated: 2024/06/16 19:50:40 by inwagner         ###   ########.fr       */
+/*   Updated: 2024/06/22 15:49:52 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,13 @@ int main( void ) {
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	
+	/*
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+	*/
 
 	return 0;
 }
--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 20:00:48 by inwagner          #+#    #+#             */
-/*   Updated: 2024/06/17 20:02:19 by inwagner         ###   ########.fr       */
+/*   Updated: 2024/06/22 16:28:26 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,95 @@ int main( void ) {
 	std::cout << b << std::endl;
 	
 	std::cout << Fixed::max( a, b ) << std::endl;
+
+	//std::cout << a / 0 << std::endl; //Gera uma exception
 	
 	return 0;
 }
+
+/*
+int main( void ) {
+	// Default constructor
+	Fixed a;
+	std::cout << "a: " << a << std::endl;
+
+	// Constructor with int
+	Fixed b(10);
+	std::cout << "b (from int): " << b << std::endl;
+
+	// Constructor with float
+	Fixed c(42.42f);
+	std::cout << "c (from float): " << c << std::endl;
+
+	// Copy constructor
+	Fixed d(b);
+	std::cout << "d (copy of b): " << d << std::endl;
+
+	// Assignment operator
+	a = c;
+	std::cout << "a (after assignment from c): " << a << std::endl;
+
+	// getRawBits and setRawBits
+	std::cout << "a raw bits: " << a.getRawBits() << std::endl;
+	a.setRawBits(123456);
+	std::cout << "a (after setRawBits): " << a << " raw bits: " << a.getRawBits() << std::endl;
+
+	// toFloat and toInt
+	std::cout << "a to float: " << a.toFloat() << std::endl;
+	std::cout << "a to int: " << a.toInt() << std::endl;
+
+	std::cout << "a raw bits: " << a.getRawBits() << std::endl;
+	std::cout << "b raw bits: " << b.getRawBits() << std::endl;
+	std::cout << "c raw bits: " << c.getRawBits() << std::endl;
+	std::cout << "d raw bits: " << d.getRawBits() << std::endl;
+
+	// Comparison operators
+	std::cout << "b > c: " << (b > c) << std::endl;
+	std::cout << "b < c: " << (b < c) << std::endl;
+	std::cout << "b >= d: " << (b >= d) << std::endl;
+	std::cout << "b <= d: " << (b <= d) << std::endl;
+	std::cout << "b == d: " << (b == d) << std::endl;
+	std::cout << "b != c: " << (b != c) << std::endl;
+
+	// Arithmetic operators
+	Fixed e = b + c;
+	std::cout << "b + c: " << e << std::endl;
+	Fixed f = c - b;
+	std::cout << "c - b: " << f << std::endl;
+	Fixed g = b * c;
+	std::cout << "b * c: " << g << std::endl;
+	Fixed h = c / b;
+	std::cout << "c / b: " << h << std::endl;
+
+	// Increment and decrement operators
+	Fixed i;
+	std::cout << "i: " << i << std::endl;
+	std::cout << "++i: " << ++i << std::endl;
+	std::cout << "i: " << i << std::endl;
+	std::cout << "i++: " << i++ << std::endl;
+	std::cout << "i: " << i << std::endl;
+	std::cout << "--i: " << --i << std::endl;
+	std::cout << "i: " << i << std::endl;
+	std::cout << "i--: " << i-- << std::endl;
+	std::cout << "i: " << i << std::endl;
+
+	// Min and max functions
+	std::cout << "min(b, c): " << Fixed::min(b, c) << std::endl;
+	std::cout << "max(b, c): " << Fixed::max(b, c) << std::endl;
+
+	// Testing the given example
+	Fixed const j(Fixed(5.05f) * Fixed(2));
+	std::cout << "j: " << j << std::endl;
+
+	std::cout << "a: " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a: " << a << std::endl;
+
+	std::cout << "b: " << b << std::endl;
+	std::cout << "Fixed::max(a, j): " << Fixed::max(a, j) << std::endl;
+
+	return 0;
+}
+*/
